e40d3656ec9338391c7cc310daa44f25b64ef944
Bruno Krebs <bruno.krebs@auth0.com>
2018-09-21 12:39:36 -0300

    ### Wrapping Up your MobX App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {decorate, observable, action, computed} from 'mobx';
+import Store from './Store';
+import Form from './components/Form';
+import Dashboard from './components/Dashboard';
+import Reviews from './components/Reviews';
 
 decorate(Store, {
   reviewList: observable,
@@ -10,17 +14,15 @@ decorate(Store, {
   reviewCount: computed
 });
 
+const reviewStore = new Store();
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Form store={reviewStore}/>
+        <Dashboard store={reviewStore}/>
+        <Reviews store={reviewStore}/>
       </div>
     );
   }
